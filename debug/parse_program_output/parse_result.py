c22850c43fdf31100409396d90d44d54c4abc34a
Maksim-Sebelev <7905111@list.ru>
2025-11-12 17:26:44 +0300

    Add parsing of exit codes in e2e tests.

--- debug/parse_program_output/parse_result.py
+++ debug/parse_program_output/parse_result.py
@@ -31,9 +31,31 @@ def extract_numbers(text):
 def main():
     if len(sys.argv) != 4:
         print(f"Usage: {sys.argv[0]} <paracl_exe> <test>.pcl <answer>.ans")
-        sys.exit(1)
+        return 1
 
     executable, test_input, test_answer = sys.argv[1:4]
+
+    expect_death = False
+    exit_code = 0
+
+    try:
+        with open(test_answer, 'r') as f:
+            answer_content = f.read()
+
+        if answer_content.partition(':')[0] == "DEATH_WITH":
+            expect_death = True
+            if len(answer_content.partition(':')) <= 2:
+                color_print(Colors.RED, "bad format of ans file")
+                return 1
+
+            exit_code = int(answer_content.partition(':')[2])
+
+        else:
+            answer_content.strip()
+
+    except Exception as e:
+        color_print(Colors.RED, f"Error reading file: {e}")
+        sys.exit(1)
 
     try:
         with open(test_input, 'r') as f:
@@ -44,29 +66,36 @@ def main():
                 text=True
             )
 
-        if result.returncode != 0:
+
+        if expect_death:
+            if result.returncode == exit_code:
+                color_print(Colors.GREEN, "TEST PASSED")
+                return 0
+
+            color_print(Colors.WHITE, "Expect DEATH", end = '\n\n')
+            color_print(Colors.GREEN, f"[ expected exit code ]: {exit_code}")
+            color_print(Colors.RED,   f"[ program  exit code ]: {result.returncode}", end = '\n\n')
+            color_print(Colors.RED, "\n\nTEST FAILED")
+            return 1
+
             color_print(Colors.RED, f"Error: Program failed with exit code {result.returncode}")
             print(f"stderr: {result.stderr}")
             print(f"stdout: {result.stdout}")
-            sys.exit(1)
+
+            color_print(Colors.RED, "\n\nTEST FAILED")
+
+            return 1
 
         program_stdout = result.stdout
         program_stderr = result.stderr
 
-    except Exception as e:
-        color_print(Colors.RED, f"Error running program: {e}")
-        sys.exit(1)
-
-    try:
-        with open(test_answer, 'r') as f:
-            answer_content = f.read().strip()
-        
         expected_numbers = extract_numbers(answer_content)        
         program_output = extract_numbers(program_stdout)
 
     except Exception as e:
-        color_print(Colors.RED, f"Error reading files: {e}")
-        sys.exit(1)    
+        color_print(Colors.RED, f"Error reading file: {e}")
+        sys.exit(1)
+
     if program_stderr:
         print(f"{Colors.YELLOW}Program stderr:{Colors.RESET}")
         print(program_stderr)
